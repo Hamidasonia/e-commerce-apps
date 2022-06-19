a765d6030e2609e7badcb2526ae332ffe6406bd7
hamida <hamidadun@gmail.com>
2022-06-19 22:07:13 +0700

    Melengkapi UI + Integrated API (Public) Login Register

--- lib/page/splash_page.dart
+++ lib/page/splash_page.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
+import 'package:e_commerce_apps/api/session_manager.dart';
 import 'package:e_commerce_apps/common/constans.dart';
 import 'package:e_commerce_apps/model/app/singleton_model.dart';
+import 'package:e_commerce_apps/page/home/home_page.dart';
+import 'package:e_commerce_apps/page/main_page.dart';
 import 'package:e_commerce_apps/page/onboard_page.dart';
 import 'package:e_commerce_apps/tool/permissions_service.dart';
 import 'package:flutter/material.dart';
@@ -23,13 +26,14 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 5),
-    )..addListener(() {
+    )
+      ..addListener(() {
         setState(() {});
       });
     _controller.forward();
     _model = SingletonModel.withContext(context);
     _checkPermission();
-    _startSplash();
+    _cekSession();
   }
 
   void _checkPermission() async {
@@ -39,6 +43,8 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
 
   void _checkData() {
     _model.item = [];
+    _model.isLogin = false;
+    _model.user.data.users = [];
   }
 
   @override
@@ -47,12 +53,19 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
     super.dispose();
   }
 
-  _startSplash() {
-    var duration = const Duration(milliseconds: 5900);
-    return Timer(duration, () {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
-        return const OnBoardPage();
-      }));
+  Future _cekSession() async {
+    return Future.delayed(const Duration(milliseconds: 5900), () {
+      session.getSession().then((value) {
+        if (value != null) {
+          Navigator.pushAndRemoveUntil(
+              context, MaterialPageRoute(builder: (_) => MainPage()), (
+              route) => false);
+        } else {
+          Navigator.pushAndRemoveUntil(
+              context, MaterialPageRoute(builder: (_) => OnBoardPage()), (
+              route) => false);
+        }
+      });
     });
   }
 
